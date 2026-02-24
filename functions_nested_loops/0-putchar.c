e6e26dc2d6ccbd1f057f6d0c408577ecdb905896
Pierre1200 <rouvellat.p@icloud.com>
2026-02-24 15:45:47 +0100

    Add _putchar program and update README

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char mot[] = "_putchar";
+int i;
+for (i = 0; mot[i] != '\0'; i++)
+_putchar(mot[i]);
+_putchar('\n');
+return (0);
+}
