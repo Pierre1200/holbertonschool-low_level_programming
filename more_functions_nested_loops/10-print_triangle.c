b6c72a623680c705a0245b5d1b3dadb573b85112
Pierre1200 <rouvellat.p@icloud.com>
2026-02-27 14:50:42 +0100

    10 triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - imprime un triangle
+ * @size: taille
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
