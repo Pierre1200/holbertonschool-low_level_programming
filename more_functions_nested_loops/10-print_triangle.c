bdb1bd64bbab598a60d3241e197321d28aafd5dc
Pierre1200 <rouvellat.p@icloud.com>
2026-02-27 14:54:50 +0100

    maj triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,12 @@ void print_triangle(int size)
 {
 int i, j, k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -21,5 +27,6 @@ int i, j, k;
 			_putchar('#');
 		}
 	_putchar('\n');
+	}
 	}
 }
