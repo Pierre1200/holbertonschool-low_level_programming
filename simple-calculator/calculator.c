877171b4afebd93285b2638648a411159e3930a7
Pierre1200 <rouvellat.p@icloud.com>
2026-03-01 20:40:06 +0100

    maj choix menu

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -8,20 +8,28 @@
 int main(void)
 {
 int choice = -1;
-	while (choice != 0)
-	{
-		printf("Simple Calculator\n");
+printf("Simple Calculator\n");
 		printf("1) Add\n");
 		printf("2) Subtract\n");
 		printf("3) Multiply\n");
 		printf("4) Divide\n");
 		printf("0) Quit\n");
+	while (choice != 0)
+	{
 		printf("Choice: ");
 		scanf("%d", &choice);
 		if (choice == 0)
 		{
 			printf("Bye!\n");
 		}
+			else if (choice >= 1 && choice <= 4)
+			{
+
+			}
+				else
+				{
+					printf("Invalid choice\n");
+				}
 	}
 return (0);
 }
