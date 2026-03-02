644972d3abf20484161e8cf8fe39e1d6f2ccfd21
Pierre1200 <rouvellat.p@icloud.com>
2026-03-02 12:40:14 +0100

    maj

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -9,17 +9,15 @@ static int dataset[DATASET_SIZE];
 static unsigned int next_value(unsigned int *state)
 {
 	*state = (*state * 1103515245u) + 12345u;
-	return (*state);
+	return *state;
 }
 
 static void build_dataset(void)
 {
 	unsigned int state;
-
 	int i;
 
 	state = SEED_VALUE;
-
 	for (i = 0; i < DATASET_SIZE; i++)
 		dataset[i] = (int)(next_value(&state) % 100000);
 }
@@ -27,7 +25,6 @@ static void build_dataset(void)
 static void process_dataset(void)
 {
 	int i;
-
 	int v;
 
 	for (i = 0; i < DATASET_SIZE; i++)
@@ -43,53 +40,40 @@ static void process_dataset(void)
 static unsigned long reduce_checksum(void)
 {
 	unsigned long sum;
-
 	int i;
 
 	sum = 0;
 	for (i = 0; i < DATASET_SIZE; i++)
 		sum = (sum * 131ul) + (unsigned long)dataset[i];
-
-	return (sum);
+	return sum;
 }
 
 int main(void)
 {
 	unsigned long checksum;
+	clock_t t_s, t_e, p_s, p_e;
+	double d_t, d_b, d_p, d_r;
 
-	clock_t t_start, t_end, p_start, p_end;
-	double total_t, build_t, process_t, reduce_t;
-
-	t_start = clock();
-
-	/* Mesure BUILD_DATA */
-	p_start = clock();
+	t_s = clock();
+	p_s = clock();
 	build_dataset();
-	p_end = clock();
-	build_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
-
-	/* Mesure PROCESS */
-	p_start = clock();
+	p_e = clock();
+	d_b = (double)(p_e - p_s) / CLOCKS_PER_SEC;
+	p_s = clock();
 	process_dataset();
-	p_end = clock();
-	process_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
-
-	/* Mesure REDUCE */
-	p_start = clock();
+	p_e = clock();
+	d_p = (double)(p_e - p_s) / CLOCKS_PER_SEC;
+	p_s = clock();
 	checksum = reduce_checksum();
-	p_end = clock();
-	reduce_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
-
-	t_end = clock();
-	total_t = (double)(t_end - t_start) / CLOCKS_PER_SEC;
-
+	p_e = clock();
+	d_r = (double)(p_e - p_s) / CLOCKS_PER_SEC;
+	t_e = clock();
+	d_t = (double)(t_e - t_s) / CLOCKS_PER_SEC;
 	if (checksum == 0ul)
 		printf("impossible\n");
-
-	printf("TOTAL seconds: %.6f\n", total_t);
-	printf("BUILD_DATA seconds: %.6f\n", build_t);
-	printf("PROCESS seconds: %.6f\n", process_t);
-	printf("REDUCE seconds: %.6f\n", reduce_t);
-
+	printf("TOTAL seconds: %.6f\n", d_t);
+	printf("BUILD_DATA seconds: %.6f\n", d_b);
+	printf("PROCESS seconds: %.6f\n", d_p);
+	printf("REDUCE seconds: %.6f\n", d_r);
 	return (0);
 }
