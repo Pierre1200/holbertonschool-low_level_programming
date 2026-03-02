5b51176d9287e3ccb7eb8f8499ea797ac654b2ab
Pierre1200 <rouvellat.p@icloud.com>
2026-03-02 11:55:00 +0100

    maj

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -6,36 +6,28 @@
 
 static int dataset[DATASET_SIZE];
 
-/**
- * next_value - generates the next pseudo-random value
- * @state: pointer to the current state
- * Return: next value
- */
 static unsigned int next_value(unsigned int *state)
 {
 	*state = (*state * 1103515245u) + 12345u;
-	return *state;
+	return (*state);
 }
 
-/**
- * build_dataset - fills the array
- */
 static void build_dataset(void)
 {
 	unsigned int state;
+
 	int i;
 
 	state = SEED_VALUE;
+
 	for (i = 0; i < DATASET_SIZE; i++)
 		dataset[i] = (int)(next_value(&state) % 100000);
 }
 
-/**
- * process_dataset - processes the array
- */
 static void process_dataset(void)
 {
 	int i;
+
 	int v;
 
 	for (i = 0; i < DATASET_SIZE; i++)
@@ -48,59 +40,56 @@ static void process_dataset(void)
 	}
 }
 
-/**
- * reduce_checksum - calculates checksum
- * Return: the checksum
- */
 static unsigned long reduce_checksum(void)
 {
 	unsigned long sum;
+
 	int i;
 
 	sum = 0;
 	for (i = 0; i < DATASET_SIZE; i++)
 		sum = (sum * 131ul) + (unsigned long)dataset[i];
-	return sum;
+
+	return (sum);
 }
 
-/**
- * main - entry point
- * Return: Always 0
- */
 int main(void)
 {
 	unsigned long checksum;
-	clock_t t_total_start, t_total_end;
-	clock_t t_phase_start, t_phase_end;
-	double d_total, d_build, d_process, d_reduce;
 
-	t_total_start = clock();
+	clock_t t_start, t_end, p_start, p_end;
+	double total_t, build_t, process_t, reduce_t;
 
-	t_phase_start = clock();
+	t_start = clock();
+
+	/* Mesure BUILD_DATA */
+	p_start = clock();
 	build_dataset();
-	t_phase_end = clock();
-	d_build = (double)(t_phase_end - t_phase_start) / CLOCKS_PER_SEC;
+	p_end = clock();
+	build_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
 
-	t_phase_start = clock();
+	/* Mesure PROCESS */
+	p_start = clock();
 	process_dataset();
-	t_phase_end = clock();
-	d_process = (double)(t_phase_end - t_phase_start) / CLOCKS_PER_SEC;
+	p_end = clock();
+	process_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
 
-	t_phase_start = clock();
+	/* Mesure REDUCE */
+	p_start = clock();
 	checksum = reduce_checksum();
-	t_phase_end = clock();
-	d_reduce = (double)(t_phase_end - t_phase_start) / CLOCKS_PER_SEC;
+	p_end = clock();
+	reduce_t = (double)(p_end - p_start) / CLOCKS_PER_SEC;
 
-	t_total_end = clock();
-	d_total = (double)(t_total_end - t_total_start) / CLOCKS_PER_SEC;
+	t_end = clock();
+	total_t = (double)(t_end - t_start) / CLOCKS_PER_SEC;
 
 	if (checksum == 0ul)
 		printf("impossible\n");
 
-	printf("TOTAL seconds: %.6f\n", d_total);
-	printf("BUILD_DATA seconds: %.6f\n", d_build);
-	printf("PROCESS seconds: %.6f\n", d_process);
-	printf("REDUCE seconds: %.6f\n", d_reduce);
+	printf("TOTAL seconds: %.6f\n", total_t);
+	printf("BUILD_DATA seconds: %.6f\n", build_t);
+	printf("PROCESS seconds: %.6f\n", process_t);
+	printf("REDUCE seconds: %.6f\n", reduce_t);
 
-	return 0;
+	return (0);
 }
