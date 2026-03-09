619ad07066d7ec992b8dd108d3333e90f6bd4335
Pierre1200 <rouvellat.p@icloud.com>
2026-03-09 16:44:33 +0100

    add _strdup function to duplicate a string and update main.h

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string to duplicate
+ *
+ * Return: a pointer to the duplicated string, or NULL if insufficient memory
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, lon = 0;
+	char *ptr;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str[lon] != '\0')
+	{
+		lon++;
+	}
+	ptr = malloc(sizeof(char) * (lon + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+	ptr[i] = str[i];
+	i++;
+	}
+ptr[i] = '\0';
+return (ptr);
+}
