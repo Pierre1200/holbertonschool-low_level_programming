a88b70020f2745da9fd55bd609cf6024e5439850
Pierre1200 <rouvellat.p@icloud.com>
2026-03-10 14:58:10 +0100

    add array_range function to create an array of integers and update main.h

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: the minimum value (included)
+ * @max: the maximum value (included)
+ *
+ * Return: pointer to the newly created array, or NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	size = (max - min) + 1;
+
+	ptr = malloc(size * sizeof(int));
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = min + i;
+	return (ptr);
+}
