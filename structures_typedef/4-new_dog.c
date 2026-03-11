9742aeefb5c8715d1dc0dd659bf5437b176b377d
Pierre1200 <rouvellat.p@icloud.com>
2026-03-11 17:54:22 +0100

    add new_dog function to create a new dog and update dog.h

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
+ * _strcpy - Copies a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog (Success), or NULL (Fail)
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+
+	dog->age = age;
+
+	return (dog);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,7 +6,7 @@
  * @name: Pointer to a character string representing the dog's name
  * @age: The age of the dog, as a float
  * @owner: Pointer to a character string representing the owner's name
- *
+ * dog_t - Typedef for struct dog
  * Description: This structure defines the essential attributes of a dog
  * within our veterinary database system. It stores the dog's identity,
  * its age, and its owner's identity.
@@ -19,6 +19,9 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
